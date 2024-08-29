59720d2e66b8087478c9c6e4ebdde3cf951ef5bd
hernan-vela <hernan.velamar@gmail.com>
2024-08-30 09:56:25 +1000

    First commit of Aamod repo, downloaded from Github (APaud3l)

---
+++ controllers/cli_controllers.py
@@ -0,0 +1,94 @@
+from datetime import date
+
+from flask import Blueprint
+from init import db, bcrypt
+from models.user import User
+from models.card import Card
+
+db_commands = Blueprint("db", __name__)
+
+@db_commands.cli.command("create")
+def create_tables():
+    db.create_all()
+    print("Tables created!")
+
+@db_commands.cli.command("seed")
+def seed_tables():
+    # Create a list of User instances
+    users = [
+        User(
+            email = "admin@email.com",
+            password = bcrypt.generate_password_hash("123456").decode("utf-8"),
+            is_admin = True
+        ), 
+        User(
+            name = "User A",
+            email = "usera@email.com",
+            password = bcrypt.generate_password_hash("123456").decode("utf-8")
+        )
+    ]
+
+    db.session.add_all(users)
+
+    cards = [
+        Card(
+            title = "Github Operations",
+            description = "Perform mandatory github ops on the project",
+            status = "To Do",
+            priority = "High",
+            date = date.today(),
+            user = users[0]
+        ), 
+        Card(
+            title = "Initialise the modules",
+            description = "Perform init operations on the necessary modules",
+            status = "Ongoing",
+            priority = "High",
+            date = date.today(),
+            user = users[0]            
+        ), Card(
+            title = "Add comments to the code",
+            description = "Add meaningful comments where necessary",
+            status = "To Do",
+            priority = "Low",
+            date = date.today(),
+            user = users[1]
+        )]
+
+    db.session.add_all(cards)
+
+    comments = [
+        Comment(
+            date = date.today(),
+            user = users[0],
+            card = cards[0],
+            message = "Admin is making comment on Card 0"
+        ),
+        Comment (
+            date = date.today(),
+            user = users[0],
+            card = cards[1],
+            message = "Admin is making comment on Card 1"
+        ),
+        Comment (
+            date = date.today(),
+            user = users[1],
+            card = cards[0],
+            message = "User A is making comment on Card 0"
+        )
+    ]
+
+    db.session.add_all(comments)
+
+    db.session.add_all(cards)
+
+    db.session.commit()
+
+    print("Tables seeded!")
+
+@db_commands.cli.command("drop")
+def drop_tables():
+    db.drop_all()
+    print("Tables droppped.")
+
+
